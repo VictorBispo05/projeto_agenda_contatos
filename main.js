9eaa9acc3c8d1c6ac8dfa0f7bae23c4d9323f241
Victor Bispo <VictorBispo05@outlook.com>
2023-09-13 11:30:46 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form-atividade')
+const atividades = [];
+const notas = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade')
+    const inputNotaAtividade = document.getElementById('nota-atividade')
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`)
+    } else {
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `</tr>`;
+
+        linhas += linha;
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
